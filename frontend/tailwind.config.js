2816e246aa5fcc6600cffef67730d7b275dece54
mashharuki <avp.104.106.107@ae.auone-net.jp>
2023-02-25 00:32:53 +0900

    学習コンテンツの見た目を修正しました。

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   mode: 'jit',
   darkMode: 'class',
@@ -14,5 +17,12 @@ module.exports = {
   },
   plugins: [
     require('@tailwindcss/aspect-ratio'),
+    plugin(function({ addBase, theme }) {
+      addBase({
+        'h1': { fontSize: theme('fontSize.2xl') },
+        'h2': { fontSize: theme('fontSize.xl') },
+        'h3': { fontSize: theme('fontSize.lg') },
+      })
+    })
   ],
 }
