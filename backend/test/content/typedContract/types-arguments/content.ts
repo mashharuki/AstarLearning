f41adf1b5c369d09fef78e833e599214cfd349da
mashharuki <avp.104.106.107@ae.auone-net.jp>
2023-03-26 21:56:19 +0900

    コンテンツ用のコントラクトをコンパイルできました！

---
+++ backend/test/content/typedContract/types-arguments/content.ts
@@ -0,0 +1,12 @@
+import type BN from 'bn.js';
+
+export type ContentInfo = {
+	contentId: (number | string | BN),
+	title: string,
+	content: string,
+	goods: (number | string | BN),
+	quizs: Array<string>,
+	answer: (number | string | BN),
+	imageurl: string
+}
+
---
+++ backend/test/nft/typedContract/mixed-methods/content.ts
@@ -0,0 +1,50 @@
+/* This file is auto-generated */
+
+import type { ContractPromise } from '@polkadot/api-contract';
+import type { ApiPromise } from '@polkadot/api';
+import type { KeyringPair } from '@polkadot/keyring/types';
+import type { GasLimit, GasLimitAndRequiredValue, Result } from '@supercolony/typechain-types';
+import type { QueryReturnType } from '@supercolony/typechain-types';
+import { queryOkJSON, queryJSON, handleReturnType } from '@supercolony/typechain-types';
+import { txSignAndSend } from '@supercolony/typechain-types';
+import type * as ArgumentTypes from '../types-arguments/content';
+import type * as ReturnTypes from '../types-returns/content';
+import type BN from 'bn.js';
+import {ReturnNumber} from '@supercolony/typechain-types';
+import {getTypeDescription} from './../shared/utils';
+// @ts-ignore
+import type {EventRecord} from "@polkadot/api/submittable";
+import {decodeEvents} from "../shared/utils";
+
+
+export default class Methods {
+	private __nativeContract : ContractPromise;
+	private __keyringPair : KeyringPair;
+	private __callerAddress : string;
+	private __apiPromise: ApiPromise;
+
+	constructor(
+		apiPromise : ApiPromise,
+		nativeContract : ContractPromise,
+		keyringPair : KeyringPair,
+	) {
+		this.__apiPromise = apiPromise;
+		this.__nativeContract = nativeContract;
+		this.__keyringPair = keyringPair;
+		this.__callerAddress = keyringPair.address;
+	}
+
+	/**
+	* getContents
+	*
+	* @returns { void }
+	*/
+	"getContents" (
+		__options: GasLimit,
+	){
+		return txSignAndSend( this.__apiPromise, this.__nativeContract, this.__keyringPair, "getContents", (events: EventRecord) => {
+			return decodeEvents(events, this.__nativeContract, "content");
+		}, [], __options);
+	}
+
+}
